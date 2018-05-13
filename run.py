15864eb42a444df55c0c62862f1918dbd4dc692f
adrian <adrian.steffan@hotmail.de>
2018-05-13 15:54:25 +0200

    Basic connect/disconnect functionality

--- run.py
+++ run.py
@@ -2,55 +2,186 @@ import discord
 from discord.ext import commands
 import youtube_dl
 import asyncio
+from collections import deque
 
 
-with open('config/config.txt') as f:
-    config = f.read().splitlines()
+"""
+Backlog:
+
+core:
+    Playlist fucntionality + change name back
+    on guild join event
+
+music integration:
+    spotify
+    Song info
+
+cleanup:
+    split files
+    hide ffmpeg
+
+documentation:
+    readme
+    help
+
+
+"""
+
+token = "NDQ0ODA4NjMyNzA2NjYyNDAw.DdhXTw.TgIgkWS6qsdeNSECFqsj1Zo8vCM"
+DEFAULT_NICKNAME = "{DiscordJockey}"
+NO_GUILD_MESSAGE = 'Warining: Please join a voice channel or enter the command in guild chat'
+NOT_CONNECTED_MESSAGE = "Error: Bot not connected to any voice channel"
+CHANNEL_NOT_FOUND_MESSAGE = "Error: Could not find channel "
+
+
+class AudioController:
     
-serverID = config[0].split(':')[1]
-textID = config[1].split(':')[1]
-voiceID = config[2].split(':')[1]
-token = config[3].split(':')[1]
+    def __init__(self,guild,volume):
+        self._volume = volume
+        self.playlist = Playlist()
+        self.current_songinfo = None
+        self.guild = guild
+
+    @property
+    def volume(self):
+        return self._volume
+
+    @volume.setter
+    def volume(self, value):
+        self._volume = value
+        try:
+            self.guild.voice_client.source.volume = float(value)/100.0
+        except:
+            pass
+
+    def next_song(self):
+        pass
+
+    def add_song(self):
+       pass
+
+    async def play_youtube(self,youtube_link):
+
+        ##fix it
+        try:
+            downloader = youtube_dl.YoutubeDL({'format': 'bestaudio','title':True})
+            extracted_info = downloader.extract_info(youtube_link, download=False)
+        except:
+            downloader = youtube_dl.YoutubeDL({})
+            extracted_info = downloader.extract_info(youtube_link, download=False)
+    
+        new_nick = ("[" +extracted_info.get('title').split("(")[0])[0:29]+ "]"
+        await self.guild.me.edit(nick=new_nick)
+    
+        self.guild.voice_client.play(discord.FFmpegPCMAudio(extracted_info['url']), after = self.next_song())
+        self.guild.voice_client.source = discord.PCMVolumeTransformer(self.guild.voice_client.source)
+        self.guild.voice_client.source.volume = float(self.volume)/100.0
+
+    
+class Playlist:
+
+    MAX_HISTORY_LENGTH = 10
+    
+    def __init__(self):
+        self.playque = deque()
+        self.playhistory = deque()
+    
 
 
-bot = commands.Bot(command_prefix="!")
-no_guild_message = 'Please join a voice channel or enter the command in guild chat'
+
+
+bot = commands.Bot(command_prefix="!",pm_help=True)
+
+
+guild_to_audiocontroller = {}
 
 # gets the guild from a message
 def get_guild(message):
     if not message.guild is None:
         return message.guild
     for guild in bot.guilds:
-        for channel in guild.channels:
-            if channel.category != None and (channel.category.name == "Voice Channels" or channel.category.name == "Sprach-Kanäle") and message.author in channel.members:
+        for channel in guild.voice_channels:
+            if message.author in channel.members:
                 return guild
     return None
+
+async def send_message(ctx,message):
+    await ctx.send("```\n"+message+"\n```")
+
+async def connect_to_channel(guild,dest_channel_name,ctx,switch = False,default = True):
+    for channel in guild.voice_channels:
+        if str(channel.name).strip() == str(dest_channel_name).strip():
+            if switch:
+                try:
+                    await guild.voice_client.disconnect()
+                except:
+                    await send_message(ctx,NOT_CONNECTED_MESSAGE)
+            await channel.connect()
+            return
+
+    if default:      
+        try:
+            await guild.voice_channels[0].connect()
+        except:
+            pass
+    else:
+        await send_message(ctx,CHANNEL_NOT_FOUND_MESSAGE +str(dest_channel_name))
+
+        
 
 @bot.event
 async def on_ready():
     
     print('Starting the bot...')
-    await bot.change_presence(status=discord.Status.online,activity=discord.Game(name=" Music, type !commands "))
-    await bot.get_channel(int(voiceID)).connect()
-    await bot.get_channel(444809976435965965).connect()
+    await bot.change_presence(status=discord.Status.online,activity=discord.Game(name=" Music, type !help "))
+    
     for guild in bot.guilds:
-        print(guild.voice_client)
         print(guild.name)
+        await guild.me.edit(nick=DEFAULT_NICKNAME)
+        try:
+            await guild.voice_channels[0].connect()
+        except:
+            pass
+        guild_to_audiocontroller[guild] = AudioController(guild,10)
     print('Startup complete!')
+
+@bot.event
+async def on_guild_join(guild):
+    pass
     
 
-@bot.command(name='commands')
-async def _commands(ctx):
-    await ctx.send('Coming Soon')
-
+    
 @bot.command(name='connect')
-async def _connect(ctx):
-    await ctx.send('Coming Soon')
+async def _connect(ctx ,*args):
+    current_guild = get_guild(ctx.message)
+    
+    if current_guild is None:
+        await send_message(ctx,NO_GUILD_MESSAGE)
+        return
+    
+    dest_channel_name = " ".join(args)
+    await connect_to_channel(current_guild,dest_channel_name,ctx,switch = False,default = True)
+    
 
 @bot.command(name='disconnect')
 async def _disconnect(ctx):
-    await ctx.send('Coming Soon')
+    current_guild = get_guild(ctx.message)
     
+    if current_guild is None:
+        await send_message(ctx,NO_GUILD_MESSAGE)
+        return
+    await current_guild.voice_client.disconnect()
+
+@bot.command(name='cc',aliases=["changechannel"])
+async def _connect(ctx ,*args):
+    current_guild = get_guild(ctx.message)
+    
+    if current_guild is None:
+        await send_message(ctx,NO_GUILD_MESSAGE)
+        return
+    
+    dest_channel_name = " ".join(args)
+    await connect_to_channel(current_guild,dest_channel_name,ctx,switch = True,default = False)
 
 @bot.command(name='yt')
 async def _play_youtube(ctx,youtube_link):
@@ -58,22 +189,17 @@ async def _play_youtube(ctx,youtube_link):
     current_guild = get_guild(ctx.message)
     
     if current_guild is None:
-        await ctx.send(no_guild_message)
+        await send_message(ctx,NO_GUILD_MESSAGE)
         return
-    
-    downloader = youtube_dl.YoutubeDL({'format': 'bestaudio','title':True})
-    extracted_info = downloader.extract_info(youtube_link, download=False)
-    new_nick = ("[" +extracted_info.get('title').split("(")[0])[0:29]+"]"
-    await current_guild.me.edit(nick=new_nick)
-    current_guild.voice_client.play(discord.FFmpegPCMAudio(extracted_info['url']), after=None)
-    current_guild.voice_client.source = discord.PCMVolumeTransformer(current_guild.voice_client.source)
+    audiocontroller = guild_to_audiocontroller[current_guild]
+    await audiocontroller.play_youtube(youtube_link)
 
 
 @bot.command(name='pause')
 async def _pause(ctx):
     current_guild = get_guild(ctx.message)
     if current_guild is None:
-        await ctx.send(no_guild_message)
+        await send_message(ctx,NO_GUILD_MESSAGE)
         return
     current_guild.voice_client.pause()
 
@@ -81,7 +207,7 @@ async def _pause(ctx):
 async def _stop(ctx):
     current_guild = get_guild(ctx.message)
     if current_guild is None:
-        await ctx.send(no_guild_message)
+        await send_message(ctx,NO_GUILD_MESSAGE)
         return
     current_guild.voice_client.stop()
     
@@ -97,7 +223,7 @@ async def _prev(ctx):
 async def _resume(ctx):
     current_guild = get_guild(ctx.message)
     if current_guild is None:
-        await ctx.send(no_guild_message)
+        await send_message(ctx,NO_GUILD_MESSAGE)
         return
     current_guild.voice_client.resume()
 
@@ -105,30 +231,10 @@ async def _resume(ctx):
 async def _volume(ctx,volume):
     current_guild = get_guild(ctx.message)
     if current_guild is None:
-        await ctx.send(no_guild_message)
+        await send_message(ctx,NO_GUILD_MESSAGE)
         return
+
+    guild_to_audiocontroller[current_guild].volume = volume
     
-    current_guild.voice_client.source.volume = float(volume)/100.0
-    
-
-@bot.command(name='clipvolume')
-async def _clipvolume(ctx):
-    await ctx.send('Coming Soon')
-
-@bot.command(name='uploadinfo')
-async def _uploadinfo(ctx):
-    await ctx.send('Coming Soon')
-
-@bot.command(name='upload')
-async def _upload(ctx):
-    await ctx.send('Coming Soon')
-
-###Clip functionality
-###Playlist fucntionality + change name back
-### Change Voice Channel
-###config file
-### hide ffmpeg
-### spotify
-## Song info
     
 bot.run(token)
